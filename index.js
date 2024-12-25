6e6208180a8a3a16f887543d6338d9d0a0185839
ndubs18 <nick.winkler1@gmail.com>
2024-12-24 18:25:19 -0800

    added infinite icon carousel

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ const linkedinLogo = document.getElementById("linkedin-logo");
 const githubLogo = document.getElementById("github-logo");
 const mediaLinks = document.getElementById("media-links");
 
+const scroller = document.getElementById("scroller");
+
+
+let addAnimation = () => {
+	scroller.setAttribute('data-animated', true);
+	let scrollerInner = document.querySelector(".scrollerInner");
+	let scrollerContent = Array.from(scrollerInner.children);
+
+	scrollerContent.forEach(e => {
+		const duplicated = e.cloneNode(true);
+		duplicated.setAttribute("aria-hidden", true);
+		scrollerInner.appendChild(duplicated);
+	})
+
+}
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+	addAnimation();
+}
+
 
 /*
 document.addEventListener("scroll", e => {
     let scrollPos = window.scrollY;
     
     if(window.innerWidth >= 800) {
-        if(scrollPos >= 375) {
-            githubLogo.src = "assets/github-logo-black.svg";
-        } else githubLogo.src = "assets/github-circle-svgrepo-com.svg";
+	if(scrollPos >= 375) {
+	    githubLogo.src = "assets/github-logo-black.svg";
+	} else githubLogo.src = "assets/github-circle-svgrepo-com.svg";
 
-        if(scrollPos >= 440) {
-            linkedinLogo.src = "assets/linkedin-logo-black.svg"
-        } else linkedinLogo.src = "assets/linkedin-circle-svgrepo-com.svg"
+	if(scrollPos >= 440) {
+	    linkedinLogo.src = "assets/linkedin-logo-black.svg"
+	} else linkedinLogo.src = "assets/linkedin-circle-svgrepo-com.svg"
 
-        if(scrollPos >= 453) {
-            mediaLinks.style.backgroundColor = "#0001"
-        }
-        else mediaLinks.style.backgroundColor = "#FFF1"
+	if(scrollPos >= 453) {
+	    mediaLinks.style.backgroundColor = "#0001"
+	}
+	else mediaLinks.style.backgroundColor = "#FFF1"
     }
 
     else {
-        githubLogo.src = "assets/github-circle-svgrepo-com.svg"; 
-        linkedinLogo.src = "assets/linkedin-circle-svgrepo-com.svg"
-        mediaLinks.style.backgroundColor = "none"
+	githubLogo.src = "assets/github-circle-svgrepo-com.svg"; 
+	linkedinLogo.src = "assets/linkedin-circle-svgrepo-com.svg"
+	mediaLinks.style.backgroundColor = "none"
     }
 })
 */
